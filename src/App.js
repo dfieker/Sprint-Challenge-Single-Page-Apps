9088ce02650543b6f9d49ccdf53b01cf27f57b7c
Daniel Fieker <danielmfieker@gmail.com>
2020-01-31 12:18:48 -0600

    Project Set Up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import axios from "axios";
 
 
 export default function App() {
+
+  const [list, setList] = useState([]);
+  console.log(list);
+  useEffect(() => {
+    axios
+      .get(
+        "https://rickandmortyapi.com/api/character/"
+      )
+      .then(response => {
+        console.log(response.data.results);
+        setList(response.data.results);
+      })
+      .catch(error => {
+        console.log("No data returned", error);
+      });
+  }, []);
   return (
     <main>
       <Header />
